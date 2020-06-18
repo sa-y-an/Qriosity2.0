de0fb04e64bea92f04a06acb15527d824b9be9c7
Sayan Mondal <sayan.mondal.tech@gmail.com>
2020-06-18 12:01:01 +0530

    models added

--- Qriosity/settings.py
+++ Qriosity/settings.py
@@ -125,6 +125,10 @@ STATIC_URL = '/static/'
 django_heroku.settings(locals())
 
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 try:
     from .local_settings import*
 except ImportError:
--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import StaticQuestions, AudioQuestions
 # Register your models here.
+
+admin.site.register(StaticQuestions)
+admin.site.register(AudioQuestions)
