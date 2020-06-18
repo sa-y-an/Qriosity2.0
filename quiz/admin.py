a4c34355a2fc0cf6d0833ab9957ca8dc657bf927
Sayan Mondal <sayan.mondal.tech@gmail.com>
2020-06-18 20:37:38 +0530

    Admin area names changed

--- quiz/admin.py
+++ quiz/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from .models import StaticQuestions, AudioQuestions
 # Register your models here.
 
+admin.site.site_header = "Qriosity 2.0"
+admin.site.site_title = "Qriosity 2.0"
+admin.site.index_title = "Welcome super admin"
+
+
 admin.site.register(StaticQuestions)
 admin.site.register(AudioQuestions)
