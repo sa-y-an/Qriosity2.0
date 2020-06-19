eb9fc65b46fea544ce1bc590930bdc4948a8bc88
sagar-wal <sagaragarwal612@gmail.com>
2020-06-19 18:29:04 +0530

    Add Google Login

--- Qriosity/settings.py
+++ Qriosity/settings.py
@@ -59,14 +59,15 @@ MIDDLEWARE = [
 AUTHENTICATION_BACKENDS = (
 
     'social_core.backends.facebook.FacebookOAuth2',
-
+    'social_core.backends.google.GoogleOAuth2',
     'django.contrib.auth.backends.ModelBackend',
 )
 
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 LOGIN_REDIRECT_URL = 'user'
-
+SOCIAL_AUTH_URL_NAMESPACE = 'social'
+    
 ROOT_URLCONF = 'Qriosity.urls'
 
 TEMPLATES = [
