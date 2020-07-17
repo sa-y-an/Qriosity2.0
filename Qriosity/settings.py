5f122655079bdd9a28f2c39b9e27b89f66311160
sagar-wal <sagaragarwal612@gmail.com>
2020-07-17 14:45:44 +0530

    404 files

--- Qriosity/settings.py
+++ Qriosity/settings.py
@@ -102,7 +102,7 @@ ROOT_URLCONF = 'Qriosity.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'home/templates/home')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
